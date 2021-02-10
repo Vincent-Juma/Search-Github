3f9d8cf582cfb50d85a18e9a8e22ad9903bbaf94
Vincent-Juma <vincentjuma64@gmail.com>
2021-02-11 01:25:15 +0300

    update index.html file

---
+++ src/app/services/github.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+@Injectable({
+  providedIn: "root"
+})
+export class GithubService {
+
+  private username = 'Vincent-Juma';
+  private client_id = '02fb1cb9cc7e0054c646';
+  private client_secret='ab40697c89af8c3b801f7e0f85d7a7f1e4cc0682';
+  
+  constructor(private http: HttpClient) {
+    console.log("Github service started");
+  }
+  getUser() {
+    return this.http.get(
+      "https://api.github.com/users/" +
+        this.username +
+        "?client_id=" +
+        this.client_id +
+        "&client_secret=" +
+        this.client_secret
+    );
+  }
+  getRepos() {
+    return this.http.get(
+      "https://api.github.com/users/" +
+        this.username +
+        "/repos" +
+        "?client_id=" +
+        this.client_id +
+        "&client_secret=" +
+        this.client_secret
+    );
+  }
+  updateUsername(username: any) {
+    this.username = username;
+  }
+}
