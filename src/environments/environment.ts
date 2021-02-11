7d1a72a2207b9115640688fd30dcd3c7b35dce68
Vincent-Juma <vincentjuma64@gmail.com>
2021-02-11 09:29:32 +0300

    minor changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,7 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
-export const environment = {
+export const environment = {'b40697c89af8c3b801f7e0f85d7a7f1e4cc0682'
   production: false
 };
 
@@ -13,4 +12,4 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
